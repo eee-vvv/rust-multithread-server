e76b6b9f0766d6c09ebb03d49cfc4a1021a1082c
Evan RPi <evan.w.and@gmail.com>
2021-06-27 23:36:19 -0400

    updated TcpListener URI so server running on pi can be accessed by other machines on network

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 use hello::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("192.168.1.180:7878").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
